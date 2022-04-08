90ab713bdd1a10cec1fabe14c3b036ad1ed39f04
Santhosh12072000 <santhose12072000@gmail.com>
2022-04-08 16:58:05 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -14,11 +14,16 @@ import { ListingComponent } from './listing/listing.component';
 import { HomeComponent } from './home/home.component';
 import { AddnewComponent } from './addnew/addnew.component';
 import { ProductComponent } from './product/product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule }from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
     RouterModule.forRoot([
